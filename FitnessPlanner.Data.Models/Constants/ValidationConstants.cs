3d08e00703044f933a8fcd2496c18c495270e7fb
Borislav Angelov <bigboss007@abv.bg>
2024-04-28 22:13:37 +0300

    Add validation consants for user dto

--- FitnessPlanner.Data.Models/Constants/ValidationConstants.cs
+++ FitnessPlanner.Data.Models/Constants/ValidationConstants.cs
@@ -28,8 +28,31 @@
 
         public static class UserConstants
         {
-            public const int NameMaxLength = 100;
             public const int UserIdMaxLength = 450;
+
+            public const int NameMinLength = 3;
+            public const int NameMaxLength = 100;
+            
+            public const int AgeMinLength = 14;
+            public const int AgeMaxLength = 99;
+
+            public const int PasswordMinLength = 6;
+            public const int PasswordMaxLength = 30;
+
+            public const int EmailMinLength = 5;
+            public const int EmailMaxLength = 50;
+
+            public const int HeightMinLength = 80;
+            public const int HeightMaxLength = 250;
+
+            public const int WeightMinLength = 30;
+            public const int WeightMaxLength = 200;
+        }
+        public static class ErrorMessages
+        {
+            public const string RequiredErrorMessage = "The field {0} is required.";
+            public const string StringLengthErrorMessage = "The field {0} must be between {2} and {1} characters.";
+            public const string NumberValueErrorMessage = "The {0} must be between {1} and {2}.";
         }
     }
 }
