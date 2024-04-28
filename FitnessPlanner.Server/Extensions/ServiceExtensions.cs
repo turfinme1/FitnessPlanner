83fc6f8d0ce9e2da2626fa009f711c186712f152
Borislav Angelov <bigboss007@abv.bg>
2024-04-28 19:02:53 +0300

    Add extension method for IServiceCollection and add repositories to IoC

---
+++ FitnessPlanner.Server/Extensions/ServiceExtensions.cs
@@ -0,0 +1,38 @@
+﻿using FitnessPlanner.Data;
+using FitnessPlanner.Data.Contracts;
+using FitnessPlanner.Data.Models;
+using FitnessPlanner.Data.Repositories;
+using Microsoft.AspNetCore.Identity;
+
+namespace FitnessPlanner.Server.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureIdentity(this IServiceCollection builder)
+        {
+            builder.AddIdentity<User, IdentityRole>(options =>
+                {
+                    options.Password.RequireDigit = false;
+                    options.Password.RequireLowercase = false;
+                    options.Password.RequireNonAlphanumeric = false;
+                    options.Password.RequireUppercase = false;
+                    options.Password.RequiredLength = 6;
+                    options.Password.RequireLowercase = false;
+                    options.User.RequireUniqueEmail = true;
+                    options.SignIn.RequireConfirmedEmail = false;
+                })
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+        }
+        
+        public static void ConfigureApplicationServices(this IServiceCollection builder)
+        {
+            builder.AddScoped<IExercisePerformInfoRepository, ExercisePerformInfoRepository>();
+            builder.AddScoped<IExerciseRepository, ExerciseRepository>();
+            builder.AddScoped<ISingleWorkoutRepository, SingleWorkoutRepository>();
+            builder.AddScoped<IUnitOfWork, UnitOfWork>();
+            builder.AddScoped<IWorkoutPlanRepository, WorkoutPlanRepository>();
+        }
+        
+    }
+}
