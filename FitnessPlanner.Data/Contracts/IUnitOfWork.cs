6a91df2d2959eaf5d64dc035d6862bc9695437a5
Borislav Angelov <bigboss007@abv.bg>
2024-04-28 19:02:03 +0300

    Add Unit of work pattern

---
+++ FitnessPlanner.Data/Contracts/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿namespace FitnessPlanner.Data.Contracts
+{
+    public interface IUnitOfWork
+    {
+        IWorkoutPlanRepository WorkoutPlans { get; }
+
+        ISingleWorkoutRepository SingleWorkouts { get; }
+
+        IExerciseRepository Exercises { get; }
+
+        Task SaveChangesAsync();
+    }
+}
