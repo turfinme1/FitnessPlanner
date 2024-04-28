534c2766d483e4e18563ea3fd06f2905adf2fc7c
Borislav Angelov <bigboss007@abv.bg>
2024-04-28 19:00:25 +0300

    Add repository interface with implementation

---
+++ FitnessPlanner.Data/Contracts/IRepository.cs
@@ -0,0 +1,20 @@
+﻿
+using System.Linq.Expressions;
+
+namespace FitnessPlanner.Data.Contracts
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        Task<IEnumerable<TEntity>> GetAllAsync(bool isTracked = false);
+
+        Task<IEnumerable<TEntity>> GetAllFilteredAsync(Expression<Func<TEntity, bool>> filter);
+
+        Task<TEntity?> GetByIdAsync(object id);
+
+        void Add(TEntity entity);
+
+        void Update(TEntity entity);
+
+        void Remove(TEntity entity);
+    }
+}
---
+++ FitnessPlanner.Data/Repositories/Repository.cs
@@ -0,0 +1,45 @@
+﻿using System.Linq.Expressions;
+using FitnessPlanner.Data.Contracts;
+using Microsoft.EntityFrameworkCore;
+
+namespace FitnessPlanner.Data.Repositories
+{
+    public abstract class Repository<TEntity>(ApplicationDbContext context) : IRepository<TEntity>
+        where TEntity : class
+    {
+        //protected readonly ApplicationDbContext DbContext = context;
+        protected readonly DbSet<TEntity> DbSet = context.Set<TEntity>();
+
+        public async Task<IEnumerable<TEntity>> GetAllAsync(bool isTracked = false)
+        {
+            return isTracked
+                ? await DbSet.ToListAsync()
+                : await DbSet.AsNoTracking().ToListAsync();
+        }
+
+        public async Task<IEnumerable<TEntity>> GetAllFilteredAsync(Expression<Func<TEntity, bool>> filter)
+        {
+            return await DbSet.AsNoTracking().Where(filter).ToListAsync();
+        }
+
+        public async Task<TEntity?> GetByIdAsync(object id)
+        {
+            return await DbSet.FindAsync(id);
+        }
+
+        public void Add(TEntity entity)
+        {
+            DbSet.Add(entity);
+        }
+
+        public void Update(TEntity entity)
+        {
+            DbSet.Update(entity);
+        }
+
+        public void Remove(TEntity entity)
+        {
+            DbSet.Remove(entity);
+        }
+    }
+}
