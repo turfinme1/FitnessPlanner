dbb6025c4cc663c0c34bf079d6c4426aea211691
Borislav Angelov <bigboss007@abv.bg>
2024-04-29 22:52:02 +0300

    Add Service encapsulating Azure Blob Storage operations

---
+++ FitnessPlanner.Services/FilePersistence/Contracts/IFilePersistenceService.cs
@@ -0,0 +1,8 @@
+﻿
+namespace FitnessPlanner.Services.FilePersistence.Contracts
+{
+    public interface IFilePersistenceService
+    {
+        Task AddFileAsync(Stream fileStream, string fileName);
+    }
+}
---
+++ FitnessPlanner.Services/FilePersistence/FilePersistenceService.cs
@@ -0,0 +1,28 @@
+﻿using Azure.Storage.Blobs;
+using FitnessPlanner.Services.FilePersistence.Contracts;
+using Microsoft.Extensions.Logging;
+
+namespace FitnessPlanner.Services.FilePersistence
+{
+    public sealed class FilePersistenceService(
+        BlobServiceClient serviceClient,
+        ILogger<FilePersistenceService> logger) : IFilePersistenceService
+    {
+        private readonly BlobContainerClient _containerClient = serviceClient.GetBlobContainerClient("images");
+
+        public async Task AddFileAsync(Stream fileStream, string fileName)
+        {
+            var blobClient = _containerClient.GetBlobClient(fileName);
+
+            try
+            {
+                await blobClient.UploadAsync(fileStream, overwrite: true);
+            }
+            catch (Exception e)
+            {
+                logger.LogError(e, $"Error in {nameof(AddFileAsync)}");
+                throw;
+            }
+        }
+    }
+}
