2bf19ac16459ffb9c1bcda68b3bb9f25d8fcd11c
Borislav Angelov <bigboss007@abv.bg>
2024-05-11 15:24:09 +0300

    Add BodyMassIndexMeasure entity with cross table

---
+++ FitnessPlanner.Data.Models/BodyMassIndexMeasure.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using static FitnessPlanner.Data.Models.Constants.ValidationConstants.BodyMassIndexMeasureConstants;
+
+namespace FitnessPlanner.Data.Models
+{
+    [Table("body_mass_index_measure_id")]
+    public class BodyMassIndexMeasure
+    {
+        [Key]
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(TypeMaxLength)]
+        [Column("type")]
+        public required string Type { get; set; }
+
+        public IEnumerable<User> Users { get; set; } = [];
+
+        public IEnumerable<WorkoutPlanBodyMassIndexMeasure> WorkoutPlanBodyMassIndexMeasures { get; set; } = [];
+    }
+}
---
+++ FitnessPlanner.Data.Models/WorkoutPlanBodyMassIndexMeasure.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FitnessPlanner.Data.Models
+{
+    [Table("workout_plan_body_mass_index_measure")]
+    [PrimaryKey(nameof(WorkoutPlanId), nameof(BodyMassIndexMeasureId))]
+    public class WorkoutPlanBodyMassIndexMeasure
+    {
+        [Column("workout_plan_id")]
+        public int WorkoutPlanId { get; set; }
+
+        [ForeignKey(nameof(WorkoutPlanId))]
+        public WorkoutPlan WorkoutPlan { get; set; } = null!;
+
+        [Column("body_mass_index_measure_id")]
+        public int BodyMassIndexMeasureId { get; set; }
+
+        [ForeignKey(nameof(BodyMassIndexMeasureId))]
+        public BodyMassIndexMeasure BodyMassIndexMeasure { get; set; } = null!;
+    }
+}
