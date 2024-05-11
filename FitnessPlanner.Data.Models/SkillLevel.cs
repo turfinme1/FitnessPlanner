118dfac460f6b8f9cd10bd228bbf3be1e0f6ecad
Borislav Angelov <bigboss007@abv.bg>
2024-05-11 15:23:30 +0300

    Add SkillLevel entity

---
+++ FitnessPlanner.Data.Models/SkillLevel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using static FitnessPlanner.Data.Models.Constants.ValidationConstants.SkillLevelConstants;
+
+namespace FitnessPlanner.Data.Models
+{
+    [Table("skill_level")]
+    public class SkillLevel
+    {
+        [Key]
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(NameMaxLength)]
+        [Column("name")]
+        public required string Name { get; set; }
+
+        public IEnumerable<WorkoutPlan> WorkoutPlans { get; set; } = [];
+
+        public IEnumerable<User> Users { get; set; } = [];
+    }
+}
