f6b524b5f7537b1f476dc9d5d61c3d27a07eedad
Borislav Angelov <bigboss007@abv.bg>
2024-05-11 15:22:34 +0300

    Add Goal entity

---
+++ FitnessPlanner.Data.Models/Goal.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using static FitnessPlanner.Data.Models.Constants.ValidationConstants.GoalConstants;
+
+namespace FitnessPlanner.Data.Models
+{
+    [Table("goal")]
+    public class Goal
+    {
+        [Key]
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(NameMaxLength)]
+        [Column("name")]
+        public required string Name { get; set; }
+
+        public IEnumerable<WorkoutPlan> WorkoutPlans { get; set; } = [];
+
+        public IEnumerable<User> Users { get; set; } = [];
+    }
+}
