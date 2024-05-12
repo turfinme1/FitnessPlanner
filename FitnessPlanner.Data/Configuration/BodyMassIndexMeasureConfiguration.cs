88c18cfff4cd89158ca3e1cd3e2529a319af82cf
Borislav Angelov <bigboss007@abv.bg>
2024-05-12 17:03:08 +0300

    Add BodyMassIndexMeasure seed

---
+++ FitnessPlanner.Data/Configuration/BodyMassIndexMeasureConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using FitnessPlanner.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace FitnessPlanner.Data.Configuration
+{
+    public class BodyMassIndexMeasureConfiguration : IEntityTypeConfiguration<BodyMassIndexMeasure>
+    {
+        public void Configure(EntityTypeBuilder<BodyMassIndexMeasure> builder)
+        {
+            builder.HasData(GetBodyMassIndexMeasures());
+        }
+
+        private static BodyMassIndexMeasure[] GetBodyMassIndexMeasures()
+        {
+            return
+            [
+                new BodyMassIndexMeasure { Id = 1, Type = "Severe Underweight" },
+                new BodyMassIndexMeasure { Id = 2, Type = "Underweight" },
+                new BodyMassIndexMeasure { Id = 3, Type = "Normal weight" },
+                new BodyMassIndexMeasure { Id = 4, Type = "Slightly Above Normal weight" },
+                new BodyMassIndexMeasure { Id = 5, Type = "Overweight" },
+                new BodyMassIndexMeasure { Id = 6, Type = "Obesity" }
+            ];
+        }
+    }
+}
