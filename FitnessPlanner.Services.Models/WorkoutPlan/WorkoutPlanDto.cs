4c67c094a3c4d5d5a2a8abe2c4f9e14756e424e6
Borislav Angelov <bigboss007@abv.bg>
2024-05-13 21:05:05 +0300

    Add WorkoutPlan Dto

---
+++ FitnessPlanner.Services.Models/WorkoutPlan/WorkoutPlanDto.cs
@@ -0,0 +1,17 @@
+﻿using FitnessPlanner.Services.Models.SingleWorkout;
+
+namespace FitnessPlanner.Services.Models.WorkoutPlan
+{
+    public class WorkoutPlanDto
+    {
+        public int Id { get; set; }
+
+        public required string Name { get; set; }
+
+        public required string Goal { get; set; }
+
+        public required string SkillLevel { get; set; }
+
+        public required IEnumerable<SingleWorkoutDto> Workouts { get; set; }
+    }
+}
