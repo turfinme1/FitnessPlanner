1286a9ee2c38050a180444c34cc1075c567a5337
Borislav Angelov <bigboss007@abv.bg>
2024-05-13 21:07:37 +0300

    Add WorkoutPlan service with get method

---
+++ FitnessPlanner.Services/WorkoutPlan/Contracts/IWorkoutPlanService.cs
@@ -0,0 +1,9 @@
+﻿using FitnessPlanner.Services.Models.WorkoutPlan;
+
+namespace FitnessPlanner.Services.WorkoutPlan.Contracts
+{
+    public interface IWorkoutPlanService
+    {
+        Task<IEnumerable<WorkoutPlanDto>> GetAllAsync();
+    }
+}
---
+++ FitnessPlanner.Services/WorkoutPlan/WorkoutPlanService.cs
@@ -0,0 +1,38 @@
+﻿using FitnessPlanner.Data.Contracts;
+using FitnessPlanner.Services.Models.ExercisePerformInfo;
+using FitnessPlanner.Services.Models.SingleWorkout;
+using FitnessPlanner.Services.Models.WorkoutPlan;
+using FitnessPlanner.Services.WorkoutPlan.Contracts;
+using Microsoft.Extensions.Logging;
+
+namespace FitnessPlanner.Services.WorkoutPlan
+{
+    public class WorkoutPlanService(
+        IUnitOfWork repositoryManager,
+        ILogger<WorkoutPlanService> logger) : IWorkoutPlanService
+    {
+        public async Task<IEnumerable<WorkoutPlanDto>> GetAllAsync()
+        {
+                return (await repositoryManager.WorkoutPlans.GetAllWithRelatedEntitiesAsync()).Select(wp=> new WorkoutPlanDto()
+                {
+                    Id = wp.Id,
+                    Name = wp.Name,
+                    Goal = wp.Goal.Name,
+                    SkillLevel = wp.SkillLevel.Name,
+                    Workouts = wp.SingleWorkoutWorkoutPlans.Select(swwp => new SingleWorkoutDto()
+                    {
+                        Id = swwp.SingleWorkout.Id,
+                        Name = swwp.SingleWorkout.Name,
+                        Day = (int)swwp.SingleWorkout.Day,
+                        Exercises = swwp.SingleWorkout.ExercisePerformInfoSingleWorkouts.Select(episw => new ExercisePerformInfoDto()
+                        {
+                            Id = episw.ExercisePerformInfo.Id,
+                            ExerciseName = episw.ExercisePerformInfo.Exercise.Name,
+                            Sets = episw.ExercisePerformInfo.Sets,
+                            Reps = episw.ExercisePerformInfo.Reps
+                        })
+                    })
+                });
+        }
+    }
+}
