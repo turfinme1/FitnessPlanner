404fd9dfdba89dde6e6dbb1d7ff0a4884f025cfd
Borislav Angelov <bigboss007@abv.bg>
2024-05-13 21:05:39 +0300

    Add ExercisePerformInfo Dto

---
+++ FitnessPlanner.Services.Models/ExercisePerformInfo/ExercisePerformInfoDto.cs
@@ -0,0 +1,14 @@
+﻿namespace FitnessPlanner.Services.Models.ExercisePerformInfo
+{
+    public class ExercisePerformInfoDto
+    {
+        public int Id { get; set; }
+
+        public required string ExerciseName { get; set; }
+
+        public int Reps { get; set; }
+
+        public int Sets { get; set; }
+
+    }
+}
