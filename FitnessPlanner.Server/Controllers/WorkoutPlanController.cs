386c9a29b9fbdcd32620bb4f241e53a802bdcdd0
Borislav Angelov <bigboss007@abv.bg>
2024-05-13 21:09:35 +0300

    Add WorkoutPlan controller with get all action

---
+++ FitnessPlanner.Server/Controllers/WorkoutPlanController.cs
@@ -0,0 +1,33 @@
+﻿using FitnessPlanner.Services.Models.WorkoutPlan;
+using FitnessPlanner.Services.WorkoutPlan.Contracts;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FitnessPlanner.Server.Controllers
+{
+    [Route("api/workout-plan")]
+    [ApiController]
+    public class WorkoutPlanController(
+        IWorkoutPlanService workoutPlanService,
+        ILogger<WorkoutPlanController> logger) : BaseController
+    {
+
+        [HttpGet]
+        [AllowAnonymous]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<IEnumerable<WorkoutPlanDto>>> GetAllWorkoutPlans()
+        {
+            try
+            {
+                return Ok(await workoutPlanService.GetAllAsync());
+            }
+            catch (Exception e)
+            {
+                logger.LogError(e, $"Error in {nameof(WorkoutPlanController.GetAllWorkoutPlans)}");
+                return StatusCode(500, "Internal server error");
+            }
+        }
+    }
+}
