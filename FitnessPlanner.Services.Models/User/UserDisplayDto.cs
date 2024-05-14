c7321551ff2f7a9d3dd6defca2e50e0e6b065931
Borislav Angelov <bigboss007@abv.bg>
2024-05-15 00:01:12 +0300

    Add UserDisplayDto

---
+++ FitnessPlanner.Services.Models/User/UserDisplayDto.cs
@@ -0,0 +1,25 @@
+﻿namespace FitnessPlanner.Services.Models.User
+{
+    public class UserDisplayDto
+    {
+        public string Id { get; set; }
+
+        public required string Name { get; set; }
+
+        public required string Email { get; set; }
+
+        public required string Gender { get; set; }
+
+        public int Age { get; set; }
+
+        public double Height { get; set; }
+
+        public double Weight { get; set; }
+
+        public required string SkillLevel { get; set; }
+
+        public required string Goal { get; set; }
+
+        public required string BodyMassIndexMeasure { get; set; }
+    }
+}
