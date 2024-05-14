f0a16a1d5a1421e6dfd13db421c3c0751761caa4
Borislav Angelov <bigboss007@abv.bg>
2024-05-15 00:02:49 +0300

    Add Admin Controller with all user retrieval action

---
+++ FitnessPlanner.Server/Controllers/AdminController.cs
@@ -0,0 +1,32 @@
+﻿using FitnessPlanner.Services.Admin.Contracts;
+using FitnessPlanner.Services.Models.User;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FitnessPlanner.Server.Controllers
+{
+    [Route("api/admin")]
+    [ApiController]
+    public class AdminController(
+        IAdminService adminService,
+        ILogger<AdminController> logger) : BaseController
+    {
+        [HttpGet("users")]
+        [AllowAnonymous]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<IEnumerable<UserDisplayDto>>> GetAllUsers()
+        {
+            try
+            {
+                return Ok(await adminService.GetAllUsersAsync());
+            }
+            catch (Exception e)
+            {
+                logger.LogError(e, $"Error in {nameof(GetAllUsers)}");
+                return StatusCode(500, "Internal server error");
+            }
+        }
+    }
+}
