f433312367005d00d38d670ca751ba07ec299add
Borislav Angelov <borislav.m.angelov@gmail.com>
2024-05-17 20:28:42 +0300

    Add BoyMassIndexMeasure enum

---
+++ FitnessPlanner.Data.Models/Enums/BodyMassIndexMeasuresEnum.cs
@@ -0,0 +1,12 @@
+﻿namespace FitnessPlanner.Data.Models.Enums
+{
+    public enum BodyMassIndexMeasuresEnum
+    {
+        SevereUnderweight = 1,
+        Underweight = 2,
+        NormalWeight = 3,
+        SlightlyAboveNormalWeight = 4,
+        Overweight = 5,
+        Obesity = 6
+    }
+}
