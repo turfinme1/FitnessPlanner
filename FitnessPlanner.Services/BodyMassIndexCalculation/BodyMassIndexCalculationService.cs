0a48d5993b25497f354d81291503ed9aef41dee9
Borislav Angelov <borislav.m.angelov@gmail.com>
2024-05-17 20:32:07 +0300

    Add BodyMassIndexCalculation service

---
+++ FitnessPlanner.Services/BodyMassIndexCalculation/BodyMassIndexCalculationService.cs
@@ -0,0 +1,27 @@
+﻿using FitnessPlanner.Data.Models.Enums;
+using FitnessPlanner.Services.BodyMassIndexCalculation.Contracts;
+
+namespace FitnessPlanner.Services.BodyMassIndexCalculation
+{
+    public sealed class BodyMassIndexCalculationService : IBodyMassIndexCalculationService
+    {
+        public BodyMassIndexMeasuresEnum GetBodyMassIndexMeasure(double weight, double height)
+        {   
+            var bodyMassIndex = GetBodyMassIndex(weight, height);
+            return bodyMassIndex switch
+            {
+                < 16 => BodyMassIndexMeasuresEnum.SevereUnderweight,
+                >= 16 and < 19 => BodyMassIndexMeasuresEnum.Underweight,
+                >= 19 and < 25 => BodyMassIndexMeasuresEnum.NormalWeight,
+                >= 25 and < 28 => BodyMassIndexMeasuresEnum.SlightlyAboveNormalWeight,
+                >= 28 and < 35 => BodyMassIndexMeasuresEnum.Overweight,
+                _ => BodyMassIndexMeasuresEnum.Obesity
+            };
+        }
+
+        private static double GetBodyMassIndex(double weight, double height)
+        {
+            return weight / (height * height);
+        }
+    }
+}
---
+++ FitnessPlanner.Services/BodyMassIndexCalculation/Contracts/IBodyMassIndexCalculationService.cs
@@ -0,0 +1,10 @@
+﻿using FitnessPlanner.Data.Models.Enums;
+
+namespace FitnessPlanner.Services.BodyMassIndexCalculation.Contracts
+{
+    public interface IBodyMassIndexCalculationService
+    {
+        BodyMassIndexMeasuresEnum GetBodyMassIndexMeasure(double weight, double height);
+    }
+}
+
