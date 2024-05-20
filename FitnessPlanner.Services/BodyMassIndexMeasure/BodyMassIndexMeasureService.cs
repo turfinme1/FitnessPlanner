0abcb4d43882fd71d6658cb7cdb442e0be732d51
Borislav Angelov <borislav.m.angelov@gmail.com>
2024-05-20 17:33:07 +0300

    Add BodyMassIndexMeasure service with method retrieving measure names

---
+++ FitnessPlanner.Services/BodyMassIndexMeasure/BodyMassIndexMeasureService.cs
@@ -0,0 +1,24 @@
+﻿using FitnessPlanner.Data.Contracts;
+using FitnessPlanner.Services.BodyMassIndexMeasure.Contracts;
+using Microsoft.Extensions.Logging;
+
+namespace FitnessPlanner.Services.BodyMassIndexMeasure
+{
+    public sealed class BodyMassIndexMeasureService(
+        IUnitOfWork repositoryManager,
+        ILogger<BodyMassIndexMeasureService> logger) : IBodyMassIndexMeasureService
+    {
+        public async Task<IEnumerable<string>> GetAllNamesAsync()
+        {
+            try
+            {
+                return (await repositoryManager.BodyMassIndexMeasures.GetAllAsync()).Select(b => b.Type);
+            }
+            catch (Exception e)
+            {
+                logger.LogError(e, $"Error in {nameof(GetAllNamesAsync)}");
+                throw;
+            }
+        }
+    }
+}
