eb8a0180ab8a3baff45cf8bec0f3014cbef682fe
Borislav Angelov <borislav.m.angelov@gmail.com>
2024-05-20 17:24:21 +0300

    Add SkillLevel repository

---
+++ FitnessPlanner.Data/Contracts/ISkillLevelRepository.cs
@@ -0,0 +1,8 @@
+﻿using FitnessPlanner.Data.Models;
+
+namespace FitnessPlanner.Data.Contracts
+{
+    public interface ISkillLevelRepository : IRepository<SkillLevel>
+    {
+    }
+}
---
+++ FitnessPlanner.Data/Repositories/SkillLevelRepository.cs
@@ -0,0 +1,9 @@
+﻿using FitnessPlanner.Data.Contracts;
+using FitnessPlanner.Data.Models;
+
+namespace FitnessPlanner.Data.Repositories
+{
+    public sealed class SkillLevelRepository(ApplicationDbContext context) : Repository<SkillLevel>(context), ISkillLevelRepository
+    {
+    }
+}
