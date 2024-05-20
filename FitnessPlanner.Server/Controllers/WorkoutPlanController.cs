525b2bb8518ff2d0aca2e915b178bf7684f9bcc6
Borislav Angelov <borislav.m.angelov@gmail.com>
2024-05-20 17:38:06 +0300

    Add action retrieving workout plan by id

--- FitnessPlanner.Server/Controllers/WorkoutPlanController.cs
+++ FitnessPlanner.Server/Controllers/WorkoutPlanController.cs
@@ -25,7 +25,32 @@ namespace FitnessPlanner.Server.Controllers
             }
             catch (Exception e)
             {
-                logger.LogError(e, $"Error in {nameof(WorkoutPlanController.GetAllWorkoutPlans)}");
+                logger.LogError(e, $"Error in {nameof(GetAllWorkoutPlans)}");
+                return StatusCode(500, "Internal server error");
+            }
+        }
+
+        [HttpGet("{id}")]
+        [AllowAnonymous]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<WorkoutPlanDto>> GetWorkoutPlanById(int id)
+        {
+            try
+            {   
+                var workoutPlan = await workoutPlanService.GetByIdAsync(id);
+                if (workoutPlan == null)
+                {
+                    return NotFound();
+                }
+
+                return Ok(workoutPlan);
+            }
+            catch (Exception e)
+            {
+                logger.LogError(e, $"Error in {nameof(GetWorkoutPlanById)}");
                 return StatusCode(500, "Internal server error");
             }
         }
