1a5f0b28901cf272d5af07b2b7c9c32e7ae6851b
Borislav Angelov <borislav.m.angelov@gmail.com>
2024-05-20 17:33:52 +0300

    Ad method to retrieve user preferences

--- FitnessPlanner.Services/ApplicationUser/Contracts/IUserService.cs
+++ FitnessPlanner.Services/ApplicationUser/Contracts/IUserService.cs
@@ -1,6 +1,9 @@
-﻿namespace FitnessPlanner.Services.ApplicationUser.Contracts
+﻿using FitnessPlanner.Services.Models.User;
+
+namespace FitnessPlanner.Services.ApplicationUser.Contracts
 {
     public interface IUserService
     {
+        Task<UserPreferencesDto?> GetByIdAsUserPreferenceDtoAsync(string userId);
     }
 }
--- FitnessPlanner.Services/ApplicationUser/UserService.cs
+++ FitnessPlanner.Services/ApplicationUser/UserService.cs
@@ -1,12 +1,36 @@
-﻿using FitnessPlanner.Services.ApplicationUser.Contracts;
+﻿using FitnessPlanner.Data.Contracts;
+using FitnessPlanner.Services.ApplicationUser.Contracts;
+using FitnessPlanner.Services.Models.User;
+using Microsoft.Extensions.Logging;
 
 namespace FitnessPlanner.Services.ApplicationUser
 {
-    public sealed class UserService : IUserService
+    public sealed class UserService(
+        IUnitOfWork repositoryManager,
+        ILogger<UserService> logger) : IUserService
     {
-        public async Task SetOwnProperties()
+        public async Task<UserPreferencesDto?> GetByIdAsUserPreferenceDtoAsync(string userId)
         {
+            try
+            {
+                var user = await repositoryManager.Users.GetByIdAsync(userId);
+                ArgumentNullException.ThrowIfNull(user);
 
+                var userDto = new UserPreferencesDto()
+                {
+                    Id = user.Id,
+                    Goal = user.Goal.Name,
+                    SkillLevel = user.SkillLevel.Name,
+                    BodyMassIndexMeasures = user.BodyMassIndexMeasure.Type
+                };
+
+                return userDto;
+            }
+            catch (Exception e)
+            {
+                logger.LogError($"{nameof(GetByIdAsUserPreferenceDtoAsync)}: User not found exception");
+                throw;
+            }
         }
     }
 }
