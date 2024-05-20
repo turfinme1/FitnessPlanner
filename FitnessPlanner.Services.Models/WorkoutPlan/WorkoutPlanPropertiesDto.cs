fa415aa94fd9722727af6bab4970332751321757
Borislav Angelov <borislav.m.angelov@gmail.com>
2024-05-20 17:30:27 +0300

    Add WorkoutPlanProperties dto

---
+++ FitnessPlanner.Services.Models/WorkoutPlan/WorkoutPlanPropertiesDto.cs
@@ -0,0 +1,15 @@
+﻿using FitnessPlanner.Services.Models.SingleWorkout;
+
+namespace FitnessPlanner.Services.Models.WorkoutPlan
+{
+    public class WorkoutPlanPropertiesDto
+    {
+        public int Id { get; set; }
+
+        public required string Goal { get; set; }
+
+        public required string SkillLevel { get; set; }
+
+        public required IEnumerable<string> BodyMassIndexMeasures { get; set; }
+    }
+}
