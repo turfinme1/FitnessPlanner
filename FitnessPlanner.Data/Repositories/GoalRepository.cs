216e9738022fcecccd53554d52d2520391dc2ac9
Borislav Angelov <borislav.m.angelov@gmail.com>
2024-05-20 17:24:04 +0300

    Add Goal repository

---
+++ FitnessPlanner.Data/Repositories/GoalRepository.cs
@@ -0,0 +1,9 @@
+﻿using FitnessPlanner.Data.Contracts;
+using FitnessPlanner.Data.Models;
+
+namespace FitnessPlanner.Data.Repositories
+{
+    public sealed class GoalRepository(ApplicationDbContext context) : Repository<Goal>(context), IGoalRepository
+    {
+    }
+}
