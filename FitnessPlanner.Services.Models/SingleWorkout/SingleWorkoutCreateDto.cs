a7a598ba56bbb5370a1eb764f251edcd14ec31e8
Borislav Angelov <borislav.m.angelov@gmail.com>
2024-05-22 01:03:08 +0300

    Add DTOs for workout plan creation

---
+++ FitnessPlanner.Services.Models/SingleWorkout/SingleWorkoutCreateDto.cs
@@ -0,0 +1,23 @@
+﻿using FitnessPlanner.Data.Models.Enums;
+using FitnessPlanner.Services.Models.ExercisePerformInfo;
+using System.ComponentModel.DataAnnotations;
+using static FitnessPlanner.Data.Models.Constants.ValidationConstants.ErrorMessages;
+using static FitnessPlanner.Data.Models.Constants.ValidationConstants.SingleWorkoutConstants;
+
+namespace FitnessPlanner.Services.Models.SingleWorkout
+{
+    public class SingleWorkoutCreateDto
+    {
+        [Required(ErrorMessage = RequiredErrorMessage)]
+        [StringLength(NameMaxLength,
+            MinimumLength = NameMinLength,
+            ErrorMessage = StringLengthErrorMessage)]
+        public required string Name { get; set; }
+
+        [Required(ErrorMessage = RequiredErrorMessage)]
+        public DaysOfWeek Day { get; set; }
+
+        [Required(ErrorMessage = RequiredErrorMessage)]
+        public required IEnumerable<ExercisePerformInfoCreateDto> ExercisePerformInfos { get; set; }
+    }
+}
---
+++ FitnessPlanner.Services.Models/WorkoutPlan/WorkoutPlanCreateDto.cs
@@ -0,0 +1,29 @@
+﻿using FitnessPlanner.Services.Models.SingleWorkout;
+using System.ComponentModel.DataAnnotations;
+using static FitnessPlanner.Data.Models.Constants.ValidationConstants.ErrorMessages;
+using static FitnessPlanner.Data.Models.Constants.ValidationConstants.WorkoutPlanConstants;
+
+namespace FitnessPlanner.Services.Models.WorkoutPlan
+{
+    public class WorkoutPlanCreateDto
+    {
+        [Required(ErrorMessage = RequiredErrorMessage)]
+        [StringLength(NameMaxLength, 
+            MinimumLength = NameMinLength, 
+            ErrorMessage = StringLengthErrorMessage)]
+        public required string Name { get; set; }
+
+        [Required(ErrorMessage = RequiredErrorMessage)]
+        public required string UserId { get; set; }
+
+        public int SkillLevelId { get; set; }
+
+        public int GoalId { get; set; }
+
+        [Required(ErrorMessage = RequiredErrorMessage)]
+        public required IEnumerable<SingleWorkoutCreateDto> Workouts { get; set; }
+
+        [Required(ErrorMessage = RequiredErrorMessage)]
+        public required IEnumerable<int> BodyMassIndexMeasures { get; set; }
+    }
+}
