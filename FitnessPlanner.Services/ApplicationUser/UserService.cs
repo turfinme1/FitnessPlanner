6443230a6847c31451b6c3c9191bd290b88aff82
Borislav Angelov <borislav.m.angelov@gmail.com>
2024-05-24 14:46:18 +0300

    Add method updating user data and preferences

--- FitnessPlanner.Services/ApplicationUser/UserService.cs
+++ FitnessPlanner.Services/ApplicationUser/UserService.cs
@@ -1,12 +1,17 @@
 ﻿using FitnessPlanner.Data.Contracts;
+using FitnessPlanner.Data.Models;
 using FitnessPlanner.Services.ApplicationUser.Contracts;
+using FitnessPlanner.Services.BodyMassIndexCalculation.Contracts;
 using FitnessPlanner.Services.Models.User;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Logging;
 
 namespace FitnessPlanner.Services.ApplicationUser
 {
     public sealed class UserService(
         IUnitOfWork repositoryManager,
+        UserManager<User> userManager,
+        IBodyMassIndexCalculationService bodyMassCalculationService,
         ILogger<UserService> logger) : IUserService
     {
         public async Task<UserPreferencesDto?> GetByIdAsUserPreferenceDtoAsync(string userId)
@@ -28,7 +33,64 @@ namespace FitnessPlanner.Services.ApplicationUser
             }
             catch (Exception e)
             {
-                logger.LogError($"{nameof(GetByIdAsUserPreferenceDtoAsync)}: User not found exception");
+                logger.LogError($"{nameof(GetByIdAsUserPreferenceDtoAsync)}: User not found");
+                throw;
+            }
+        }
+
+        public async Task<IdentityResult> UpdateAsync(UserDataUpdateDto userPreferencesDto)
+        {   
+            try
+            {
+                var user = await userManager.FindByIdAsync(userPreferencesDto.Id);
+                if (user is null)
+                {
+                    throw new ArgumentNullException(nameof(user));
+                }
+
+                if (user.Name != userPreferencesDto.Name)
+                {
+                    user.Name = userPreferencesDto.Name;
+                }
+
+                if (user.Gender != userPreferencesDto.Gender)
+                {
+                    user.Gender = userPreferencesDto.Gender;
+                }
+
+                if (user.Age != userPreferencesDto.Age)
+                {
+                    user.Age = userPreferencesDto.Age;
+                }
+
+                if (user.Height != userPreferencesDto.Height)
+                {
+                    user.Height = userPreferencesDto.Height;
+                }
+
+                if (user.Weight != userPreferencesDto.Weight)
+                {
+                    user.Weight = userPreferencesDto.Weight;
+                }
+
+                if (user.SkillLevelId != userPreferencesDto.SkillLevelId)
+                {
+                    user.SkillLevelId = userPreferencesDto.SkillLevelId;
+                }
+
+                if (user.GoalId != userPreferencesDto.GoalId)
+                {
+                    user.GoalId = userPreferencesDto.GoalId;
+                }
+
+                user.BodyMassIndexMeasureId = bodyMassCalculationService
+                        .GetBodyMassIndexMeasureId(userPreferencesDto.Weight, userPreferencesDto.Height);
+
+                return await userManager.UpdateAsync(user);
+            }
+            catch (Exception e)
+            {
+                logger.LogError($"{nameof(UpdateAsync)}: Update failed");
                 throw;
             }
         }
