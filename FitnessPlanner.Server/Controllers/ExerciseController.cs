28c39c972f5810d44c95e30674642cf1be4b294e
Borislav Angelov <borislav.m.angelov@gmail.com>
2024-05-25 13:18:51 +0300

    Add exercise controller with action retrieving all exercises

---
+++ FitnessPlanner.Server/Controllers/ExerciseController.cs
@@ -0,0 +1,39 @@
+﻿using FitnessPlanner.Services.Exercise.Contracts;
+using FitnessPlanner.Services.Models.Exercise;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FitnessPlanner.Server.Controllers
+{
+    /// <summary>
+    /// Controller class for handling HTTP requests related to exercises.
+    /// </summary>
+    [ApiController]
+    [Route("api/exercise")]
+    public class ExerciseController(
+        IExerciseService exerciseService,
+        ILogger<ExerciseController> logger) : BaseController
+    {
+        /// <summary>
+        /// Retrieves all exercises.
+        /// </summary>
+        /// <returns>A list of <see cref="ExerciseDisplayDto"/></returns>
+        [HttpGet]
+        [AllowAnonymous]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<IEnumerable<ExerciseDisplayDto>>> GetAllExercises()
+        {
+            try
+            {
+                return Ok(await exerciseService.GetAllAsync());
+            }
+            catch (Exception e)
+            {
+                logger.LogError(e, $"Error in {nameof(GetAllExercises)}");
+                return StatusCode(500, "Internal server error");
+            }
+        }
+    }
+}
