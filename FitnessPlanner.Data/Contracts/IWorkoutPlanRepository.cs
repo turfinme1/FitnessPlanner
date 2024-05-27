30e849047a266e2edcb2dfe842e8144232b7ef84
Borislav Angelov <borislav.m.angelov@gmail.com>
2024-05-27 23:59:12 +0300

    Change return type to be IEnumerable and add xml documentation

--- FitnessPlanner.Data/Contracts/IWorkoutPlanRepository.cs
+++ FitnessPlanner.Data/Contracts/IWorkoutPlanRepository.cs
@@ -2,10 +2,29 @@
 
 namespace FitnessPlanner.Data.Contracts
 {
+    /// <summary>
+    /// Interface for the workout plan repository, defining methods for retrieving workout plans with related entities.
+    /// </summary>
     public interface IWorkoutPlanRepository : IRepository<WorkoutPlan>
     {
-        Task<List<WorkoutPlan>> GetAllWithRelatedEntitiesAsync();
+        /// <summary>
+        /// Retrieves all workout plans along with their related entities asynchronously.
+        /// </summary>
+        /// <returns>
+        /// A task that represents the asynchronous operation. 
+        /// The task result contains an enumerable collection of <see cref="WorkoutPlan"/> objects with their related entities.
+        /// </returns>
+        Task<IEnumerable<WorkoutPlan>> GetAllWithRelatedEntitiesAsync();
 
+        /// <summary>
+        /// Retrieves a <see cref="WorkoutPlan"/> by its ID along with its related entities asynchronously.
+        /// </summary>
+        /// <param name="id">The ID of the workout plan to retrieve.</param>
+        /// <param name="isTracked">Indicates whether the entity should be tracked by the context.</param>
+        /// <returns>
+        /// A task that represents the asynchronous operation. 
+        /// The task result contains the <see cref="WorkoutPlan"/> with its related entities, or null if no workout plan with the specified ID is found.
+        /// </returns>
         Task<WorkoutPlan?> GetByIdWithRelatedEntitiesAsync(int id, bool isTracked = false);
     }
 }
--- FitnessPlanner.Data/Repositories/WorkoutPlanRepository.cs
+++ FitnessPlanner.Data/Repositories/WorkoutPlanRepository.cs
@@ -4,9 +4,20 @@ using Microsoft.EntityFrameworkCore;
 
 namespace FitnessPlanner.Data.Repositories
 {
+    /// <summary>
+    /// Repository class for handling data operations related to workout plans.
+    /// </summary>
+    /// <param name="context">The database context to be used by the repository.</param>
     public sealed class WorkoutPlanRepository(ApplicationDbContext context) : Repository<WorkoutPlan>(context), IWorkoutPlanRepository
     {
-        public async Task<List<WorkoutPlan>> GetAllWithRelatedEntitiesAsync()
+        /// <summary>
+        /// Retrieves all workout plans along with their related entities asynchronously.
+        /// </summary>
+        /// <returns>
+        /// A task that represents the asynchronous operation. 
+        /// The task result contains an enumerable collection of <see cref="WorkoutPlan"/> objects with their related entities.
+        /// </returns>
+        public async Task<IEnumerable<WorkoutPlan>> GetAllWithRelatedEntitiesAsync()
         {
             return await base.DbSet
                 .Include(wp => wp.SingleWorkoutWorkoutPlans)
@@ -22,6 +33,15 @@ namespace FitnessPlanner.Data.Repositories
                 .ToListAsync();
         }
 
+        /// <summary>
+        /// Retrieves a <see cref="WorkoutPlan"/> by its ID along with its related entities asynchronously.
+        /// </summary>
+        /// <param name="id">The ID of the workout plan to retrieve.</param>
+        /// <param name="isTracked">Indicates whether the entity should be tracked by the context.</param>
+        /// <returns>
+        /// A task that represents the asynchronous operation. 
+        /// The task result contains the <see cref="WorkoutPlan"/> with its related entities, or null if no workout plan with the specified ID is found.
+        /// </returns>
         public async Task<WorkoutPlan?> GetByIdWithRelatedEntitiesAsync(int id, bool isTracked = false)
         {
             var query = base.DbSet
@@ -37,7 +57,5 @@ namespace FitnessPlanner.Data.Repositories
                 ? await query.FirstOrDefaultAsync(wp => wp.Id == id)
                 : await query.AsNoTracking().FirstOrDefaultAsync(wp => wp.Id == id);
         }
-
     }
-
 }
