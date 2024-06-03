8a724ccf5bf7d44b376726a781ca4e7f8dcf5710
Borislav Angelov <borislav.m.angelov@gmail.com>
2024-06-03 22:56:06 +0300

    Add auth service with login and register

---
+++ fitnessplanner.client/src/services/authService.js
@@ -0,0 +1,60 @@
+import { jwtDecode } from "jwt-decode";
+
+const storageState = JSON.parse(sessionStorage.getItem("authData"));
+
+export const login = async (userData) => {
+  const response = await fetch("authentication/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      //   'Authorization': `Bearer ${storageState.state.accessToken}`,
+    },
+    body: JSON.stringify(userData),
+  });
+
+  if (response.ok) {
+    const data = await response.json();
+    console.log(data);
+    return data;
+  }
+  console.log(response);
+  return null;
+};
+
+export const register = async (userData) => {
+  const response = await fetch("authentication/register", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(userData),
+  });
+
+  if (response.status === 201) {
+    // const data = await response.json();
+    // console.log(data);
+    // return data;
+    return true;
+  }
+
+  const errorArray = await response.json();
+  const errorMessage = errorArray.map((a) => a.description).join("\n");
+  throw new Error(errorMessage);
+  //   return null;
+};
+
+export const logout = async () => {
+  //   const response = await fetch('authentication/logout', {
+  //     method: 'POST',
+  //     headers: {
+  //       'Content-Type': 'application/json',
+  //       'Authorization': `Bearer ${storageState.state.accessToken}`,
+  //     },
+  //   });
+
+  //   if (response.ok) {
+  //     return true;
+  //   }
+
+  return true;
+};
