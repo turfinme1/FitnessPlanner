684c82541a8d02d57d0b2477f394ed60b96cb8c5
Borislav Angelov <borislav.m.angelov@gmail.com>
2024-06-20 00:14:25 +0300

    Add SearchBoard component

---
+++ fitnessplanner.client/src/components/searchBoard/SearchBoard.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import SearchBar from "../searchBar/SearchBar";
+import Section from "../section/Section";
+import ExerciseList from "../column/ExerciseList";
+
+const SearchBoard = () => {
+  const [cards, setCards] = useState(DEFAULT_CARDS);
+
+  return (
+    <div
+      className=""
+      crosses
+    >
+      <>
+         <SearchBar /> 
+         <ExerciseList />
+      </>
+    </div>
+  );
+};
+
+const DEFAULT_CARDS = [
+  { title: "Look into render bug in dashboard", id: "11" },
+  { title: "SOX compliance checklist", id: "21" },
+  { title: "[SPIKE] Migrate to Azure", id: "31" },
+  { title: "Document Notifications service", id: "41" },
+  { title: "Research DB options for new microservice", id: "51" },
+  { title: "Postmortem for outage", id: "61" },
+  { title: "Sync with product on Q3 roadmap", id: "71" },
+  { title: "Refactor context providers to use Zustand", id: "81" },
+  { title: "Add logging to daily CRON", id: "91" },
+  { title: "Set up DD dashboards for Lambda listener", id: "101" },
+];
+
+export default SearchBoard;
