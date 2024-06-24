a3ee1b6bb4fb764e7780729ffd8da319f426cf84
Borislav Angelov <borislav.m.angelov@gmail.com>
2024-06-24 15:31:53 +0300

    Code cleanup

--- fitnessplanner.client/src/components/section/Section.jsx
+++ fitnessplanner.client/src/components/section/Section.jsx
@@ -11,8 +11,7 @@ const Section = ({
   return (
     <div
       id={id}
-      className={`
-      relative 
+      className={`relative 
       ${
         customPaddings ||
         `py-10 lg:py-16 xl:py-20 ${crosses ? "lg:py-32 xl:py-40" : ""}`
