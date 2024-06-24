a03c70e9c21d9d9ca6893b5db4f08774927399fa
Borislav Angelov <borislav.m.angelov@gmail.com>
2024-06-24 15:34:10 +0300

    Add ListCard component

---
+++ fitnessplanner.client/src/components/card/ListCard.jsx
@@ -0,0 +1,39 @@
+import { useSortable } from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
+
+const ListCard = ({ day, name, id, isSortable }) => {
+  const { attributes, listeners, setNodeRef, transform, transition } =
+    useSortable({ id });
+
+  const style = { transition, transform: CSS.Transform.toString(transform) };
+
+  const SortableCard = () => {
+    return (
+      <div
+        ref={setNodeRef}
+        {...attributes}
+        {...listeners}
+        style={style}
+        className="cursor-grab rounded border border-neutral-700 bg-neutral-800 p-3 active:cursor-grabbing touch-none"
+      >
+        <p className="text-sm text-neutral-100">{`Day ${day} : ${name}`}</p>
+      </div>
+    );
+  };
+
+  const NonSortableCard = () => {
+    return (
+      <div
+        ref={setNodeRef}
+        style={style}
+        className="rounded border border-neutral-700 bg-neutral-800 p-3 touch-none cursor-pointer"
+      >
+        <p className="text-sm text-neutral-100">{`Day ${day} : ${name}`}</p>
+      </div>
+    );
+  };
+
+  return isSortable ? <SortableCard /> : <NonSortableCard />;
+};
+
+export default ListCard;
