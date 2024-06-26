442d8bcf6fd97a7463c092003faefe69a33ea820
Borislav Angelov <borislav.m.angelov@gmail.com>
2024-06-26 23:15:12 +0300

    Remove vertical padding

--- fitnessplanner.client/src/components/header/Header.jsx
+++ fitnessplanner.client/src/components/header/Header.jsx
@@ -35,7 +35,7 @@ const Header = () => {
         openNavigation ? "bg-n-8" : "bg-n-8/90 backdrop-blur-sm"
       }`}
     >
-      <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
+      <div className="flex items-center px-5 lg:px-7.5 xl:px-10">
         <Link className="block w-[12rem] xl:mr-8" to="/">
           <img src={fitnessPlanner} width={190} height={40} alt="Fitness planner" />
         </Link>
