db95efa815b09032f9f072d3581db454fc6d6380
Borislav Angelov <borislav.m.angelov@gmail.com>
2024-07-04 02:26:58 +0300

    Add retrieval functions

--- fitnessplanner.client/src/services/exerciseService.js
+++ fitnessplanner.client/src/services/exerciseService.js
@@ -4,3 +4,10 @@ export const getAllByMuscleGroup = async (muscleGroup) => {
     
     return result;
 }
+
+export const getExercises = async () => {
+    const response = await fetch("exercise/");
+    const result = await response.json();
+    console.log(result);
+    return result;
+  }
--- fitnessplanner.client/src/services/workoutService.js
+++ fitnessplanner.client/src/services/workoutService.js
@@ -10,4 +10,20 @@ export const getWorkoutPlans = async () => {
   const result = await response.json();
   console.log(result);
   return result;
-}
+};
+
+export const createWorkoutPlan = async (workoutPlan) => {
+  const response = await fetch("/workout-plan", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization:
+        "Bearer " +
+        JSON.parse(sessionStorage.getItem("authData")).state.accessToken,
+    },
+    body: JSON.stringify(workoutPlan),
+  });
+  const result = await response.json();
+  console.log(result);
+  return result;
+};
