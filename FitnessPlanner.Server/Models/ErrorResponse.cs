9f8fd08368fac2d1489f11f3928eb0c22e5344c6
Borislav Angelov <borislav.m.angelov@gmail.com>
2024-07-09 17:06:44 +0300

    Add error response models

---
+++ FitnessPlanner.Server/Models/ErrorResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace FitnessPlanner.Server.Models
+{
+    public class ErrorResponse
+    {
+        public required string Title { get; init; }
+
+        public required int Status { get; init; }
+
+        public required string ErrorMessage { get; init; }
+    }
+}
---
+++ FitnessPlanner.Server/Models/UnprocessableEntityResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace FitnessPlanner.Server.Models
+{
+    public class UnprocessableEntityResponse
+    {
+        public required string Title { get; init; }
+
+        public required int Status { get; init; }
+
+        public IDictionary<string, string[]> Errors { get; init; } = new Dictionary<string, string[]>();
+    }
+}
