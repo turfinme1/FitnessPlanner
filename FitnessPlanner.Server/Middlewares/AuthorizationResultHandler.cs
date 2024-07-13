ccc49cd1a023c5ef4f0b1b262228a59c6e774958
Borislav Angelov <borislav.m.angelov@gmail.com>
2024-07-13 18:18:21 +0300

    Add authorization middleware to modify the response object

---
+++ FitnessPlanner.Server/Middlewares/AuthorizationResultHandler.cs
@@ -0,0 +1,32 @@
+﻿using FitnessPlanner.Server.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authorization.Policy;
+
+namespace FitnessPlanner.Server.Middlewares
+{
+    public class AuthorizationResultHandler : IAuthorizationMiddlewareResultHandler
+    {
+        private readonly AuthorizationMiddlewareResultHandler defaultHandler = new();
+
+        /// <inheritdoc />
+        public async Task HandleAsync(
+            RequestDelegate next,
+            HttpContext context,
+            AuthorizationPolicy policy,
+            PolicyAuthorizationResult authorizeResult)
+        {
+            if (authorizeResult.Challenged)
+            {
+                var response =
+                    ApiResponse.Unauthorized("You are not authorized to perform this action.");
+
+                context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+                await context.Response.WriteAsJsonAsync(response);
+
+                return;
+            }
+
+            await defaultHandler.HandleAsync(next, context, policy, authorizeResult);
+        }
+    }
+}
